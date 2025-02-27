d9b0c3f90f73be1fe1834c96eb3f903d38ad8a65
mayiii14 <mayflorpajarillo@gmail.com>
2025-02-27 22:46:31 +0800

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,12 @@
+let firstFavNumb = 13;
+let secondFavNumb = 5;
+
+//logs the results of each comparison to the console
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
