49408e9ae505a7441b6a338c0b253c735c84e123
mayiii14 <mayflorpajarillo@gmail.com>
2025-02-27 22:48:35 +0800

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,17 @@
+let firstFavNum = 13;
+let secondFavNum = 5;
+
+//this perform the mathematical operators
+let sum = firstFavNum + secondFavNum;
+let difference = firstFavNum - secondFavNum;
+let product = firstFavNum * secondFavNum;
+let qoutient = firstFavNum / secondFavNum;
+let modulo = firstFavNum % secondFavNum;
+let exponent = firstFavNum ** secondFavNum;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(qoutient);
+console.log(modulo);
+console.log(exponent);
