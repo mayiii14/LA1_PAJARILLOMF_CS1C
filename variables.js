e3c2a354ce008dc7fabab8410014b651160edd48
mayiii14 <mayflorpajarillo@gmail.com>
2025-02-27 22:50:54 +0800

    variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+let name = 'Rizalyn';
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+let gender = 'Female';
+let twitterHandle = 'Rayzliin';
+
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
