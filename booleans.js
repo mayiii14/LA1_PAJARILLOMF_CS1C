1dff9b3e54ec14fcc907bb300d8b58a025c3b3b4
mayiii14 <mayflorpajarillo@gmail.com>
2025-02-27 22:44:40 +0800

    booleans.js

---
+++ booleans.js
@@ -0,0 +1,7 @@
+let isJsProgrammingLangauge = 'true';
+let JsHard = 'false';
+let favNumb = 13; 
+
+console.log(isJsProgrammingLangauge);
+console.log(JsHard);
+console.log(favNumb + undefined); //Add favNumb wih the value of undefined
